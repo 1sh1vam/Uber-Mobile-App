95565a45eaa75b1cbdf4deee7702ba79522d70a0
1sh1vam <shivam07.mt@gmail.com>
2022-10-24 17:11:40 +0530

    adding the keyboard avoid vieww

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { KeyboardAvoidingView, Platform } from 'react-native';
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -11,18 +12,24 @@ export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="HomeScreen"
-            component={HomeScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="MapScreen"
-            component={MapScreen}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? 'height' : 'padding'}
+          style={{ flex: 1 }}
+          keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+        >
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </KeyboardAvoidingView>
       </NavigationContainer>
     </Provider>
   );
