474c8c20b47452919c95f6ab61114712a1c959b2
1sh1vam <shivam07.mt@gmail.com>
2022-10-24 16:58:29 +0530

    work and home favorites components

--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -1,14 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { styled } from 'nativewind';
+import { Icon } from '@rneui/themed';
+
+const data = [
+  { id: '123', icon: 'home', location: 'Home', destination: 'Dadar, Mumbai' },
+  {
+    id: '456',
+    icon: 'briefcase',
+    location: 'Work',
+    destination: 'Andheri East, Mumbai',
+  },
+];
+
+const StyledPressable = styled(Pressable);
+const StyledIcon = styled(Icon);
 
 const NavFavourites = () => {
   return (
-    <View>
-      <Text>NavFavourites</Text>
-    </View>
-  )
-}
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <StyledPressable className="flex-row items-center p-5">
+          <StyledIcon
+            name={item.icon}
+            type="ionicon"
+            color="white"
+            size={18}
+            className="bg-gray-300 mr-4 p-3 rounded-full"
+          />
+          <View>
+            <Text>Hello</Text>
+          </View>
+        </StyledPressable>
+      )}
+    />
+  );
+};
 
-export default NavFavourites
+export default NavFavourites;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
