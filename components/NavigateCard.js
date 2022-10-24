a6594e4268ba09e97165deca4308fea37420c6f9
1sh1vam <shivam07.mt@gmail.com>
2022-10-24 15:21:24 +0530

    navigate card styles

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,14 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 
 const NavigateCard = () => {
   return (
-    <View>
-      <Text>NavigateCard</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <Text className="text-xl text-center py-5">Hello, Satya Puro</Text>
+      <View>
+        <GooglePlacesAutocomplete
+          placeholder="Where to?"
+          nearByPlacesApi="GooglePlacesSearch"
+          debounce={400}
+          styles={mapAutoCompleteStyles}
+        />
+      </View>
+    </SafeAreaView>
   )
 }
 
-export default NavigateCard
+export default NavigateCard;
 
-const styles = StyleSheet.create({})
+export const mapAutoCompleteStyles = StyleSheet.create({
+  container: {
+    flex: 0,
+    backgroundColor: 'white',
+    paddingTop: 20
+  },
+  textInput: {
+    backgroundColor: '#DDDDDF',
+    borderRadius: 0,
+    fontSize: 18,
+  },
+  textInputContainer: {
+    paddingHorizontal: 20,
+    paddingVertical: 0,
+  }
+})
