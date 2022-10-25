10cdc0027086de61ca0818458a5081581049c351
1sh1vam <shivam07.mt@gmail.com>
2022-10-25 19:48:46 +0530

    ride options

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,7 +1,28 @@
-import { Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { FlatList, Image, Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { styled } from 'nativewind'
 import { Icon } from '@rneui/themed';
+
+const data = [
+  {
+    id: '123',
+    title: 'Uber Go',
+    multiplier: 1,
+    image: require('../assets/UberCar.webp'),
+  },
+  {
+    id: '234',
+    title: 'uberXL',
+    multiplier: 1.2,
+    image: require('../assets/UberXL.webp'),
+  },
+  {
+    id: '566',
+    title: 'Uber Premium',
+    multiplier: 1.75,
+    image: require('../assets/UberPremium.webp'),
+  },
+];
 
 const StyledPressable = styled(Pressable);
 
@@ -14,10 +35,25 @@ const RideOptionsCard = () => {
         </StyledPressable>
         <Text className="text-center text-xl py-5">Select a Ride</Text>
       </View>
+
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <StyledPressable className="flex flex-row items-center justify-between px-10 active:bg-gray-200">
+            <View className="flex-row items-center gap-4">
+              <Image style={{ resizeMode: 'contain' }} source={item.image} className="w-[100px] h-[100px]" />
+              <View>
+                <Text className="text-xl font-semibold">{item.title}</Text>
+                <Text>Travel time...</Text>
+              </View>
+            </View>
+            <Text className="text-xl">₹300</Text>
+          </StyledPressable>
+        )}
+      />
     </SafeAreaView>
   )
 }
 
-export default RideOptionsCard
-
-const styles = StyleSheet.create({})
+export default RideOptionsCard;
