b63412d0e53ea7d1941a507f95c11e7192960dfb
1sh1vam <shivam07.mt@gmail.com>
2022-10-25 19:23:14 +0530

    rider options card header

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,11 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { styled } from 'nativewind'
+import { Icon } from '@rneui/themed';
+
+const StyledPressable = styled(Pressable);
 
 const RideOptionsCard = () => {
   return (
-    <View>
-      <Text>RideOptionsCard</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <StyledPressable className="absolute top-3 left-5 p-3 z-50">
+          <Icon name="chevron-left" type="fontawesome" />
+        </StyledPressable>
+        <Text className="text-center text-xl py-5">Select a Ride</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
