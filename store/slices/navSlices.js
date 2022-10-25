2ac29a78443ac2ba241abe994cbf2f11480b742a
1sh1vam <shivam07.mt@gmail.com>
2022-10-25 20:36:54 +0530

    selecting the travel time info

--- store/slices/navSlices.js
+++ store/slices/navSlices.js
@@ -28,4 +28,6 @@ export const selectOrigin = (state) => state.nav.origin;
 
 export const selectDestination = (state) => state.nav.destination;
 
+export const selectTravelTimeInfo = (state) => state.nav.travelTimeInfo;
+
 export default navSlice.reducer;
